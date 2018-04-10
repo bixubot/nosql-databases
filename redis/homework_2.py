80577f564e4fd1705ffe8ca88fabef1d3246ef89
Binwei Xu <bixubot@gmail.com>
2018-04-10 18:44:08 -0400

    database is not correct

--- redis/homework_2.py
+++ redis/homework_2.py
@@ -10,18 +10,33 @@ def article_vote(redis, user, article):
 
     if not datetime.datetime.fromtimestamp(redis.zscore('time:', article)) < cutoff:
         article_id = article.split(':')[-1]
-        if redis.sadd('voted:' + artical_id, user):
-            redis.zincrby('score:', VOTE_SCORE, article)
-            reids.hincrby(article, 'votes', 1)
+        if redis.sadd('voted:' + article_id, user):
+            redis.zincrby(name='score:', amount = VOTE_SCORE, value = article)
+            redis.hincrby(name=article, key='votes', amount=1)
 
-def article_downvote(redis, user, article):
-    # HOMEWORK 2
-    pass
+def article_switch_vote(redis, user, from_article, to_article):
+    from_article_id = from_article.split(':')[-1]
+    to_article_id = to_article.split(':')[-1]
+    
+    # remove up vote from article
+    redis.zincrby(name='score:', amount=-VOTE_SCORE, value=from_article)
+    redis.hincrby(name=from_article, key='votes', amount=-1)
+    redis.srem('voted:' + from_article_id, user)
+    # up vote for the article
+    redis.zincrby(name='score:', amount=VOTE_SCORE, value=to_article)
+    redis.hincrby(name=to_article, key='votes', amount=1)
+    redis.sadd('voted:' + to_article_id, user)
 
-##redis = redis.StrictRedis(host='localhost', port=6379, db=0)
-##article_vote(redis, "user:5", "article:0")
 
-# user x down votes article y
-# user m down votes article y
-# user n up votes article x
+redis = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# user up votes and switch votes
+article_vote(redis, "user:3", "article:1")
+article_vote(redis, "user:3", "article:3")
+article_switch_vote(redis, "user:2", "article:8", "article:1")
+
 # zrange command to find "Easter egg" article
+easter_egg = redis.zrangebyscore(name="score:", min=10, max=20)
+print(easter_egg)
+#easter_egg_id = easter_egg[0].split(':')[-1]
+#print(redis.hget("article:" + easter_egg_id, "link"))
